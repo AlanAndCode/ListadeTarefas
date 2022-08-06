77bca4ecadaf1e8372d833c8ca39e5ff50e28913
Alan <alan.andersonbr@gmail.com>
2022-08-06 02:25:57 -0300

    configurando banco de dados

--- app/src/main/java/com/example/listadetarefas/helper/DbHelper.java
+++ app/src/main/java/com/example/listadetarefas/helper/DbHelper.java
@@ -1,2 +1,27 @@
-package com.example.listadetarefas.helper;public class DbHelper {
+package com.example.listadetarefas.helper;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DbHelper extends SQLiteOpenHelper {
+  public static int VERSION = 1;
+  public static String NOME_DB = "DB_TAREFAS";
+  public static String TABELA_TAREFAS = "tarefas";
+    public DbHelper(Context context) {
+        super(context, NOME_DB, null, VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+      String sql = "CREATE IF NOT EXISTS " + TABELA_TAREFAS
+              + " (id INTEGER PRIMARY KEY) ";
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
 }
